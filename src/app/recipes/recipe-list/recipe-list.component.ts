14fa99dc713d1c8667f54ccef108d6558d8607a1
Angelo Bonini <abonini2000@gmail.com>
2020-05-14 13:52:04 -0300

    segunda importacao

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes:Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simply test',
+     'https://www.tecnonutrireceitas.com.br/central/Content/handlers/Receita.ashx?id=53916&tipo=r&default=s'),
+     new Recipe('A Test Recipe', 'This is a simply test',
+     'https://www.tecnonutrireceitas.com.br/central/Content/handlers/Receita.ashx?id=53916&tipo=r&default=s'),
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
